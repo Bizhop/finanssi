6a016989d401d8723b2c3fc1dd85df1dbc1519b1
Bizhop <ville.piispa@gmail.com>
2015-02-26 21:36:57 +0200

    another hash stuff

--- src/main/java/finanssi/util/Hasher.java
+++ src/main/java/finanssi/util/Hasher.java
@@ -12,7 +12,12 @@ public class Hasher {
         try {
             MessageDigest md = MessageDigest.getInstance("MD5");
             md.update(password.getBytes());
-            return String.valueOf(Base64.getEncoder().encode(md.digest()));
+            byte[] bytes = md.digest();
+            StringBuilder sb = new StringBuilder();
+            for(byte b : bytes) {
+                sb.append(Integer.toString((b & 0xff) + 0x100, 16).substring(1));
+            }
+            return sb.toString();
         } catch (NoSuchAlgorithmException e) {
             //fuck off, this doesn't happen
             e.printStackTrace();
