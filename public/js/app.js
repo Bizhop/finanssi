6f47fb521e126eb0e15ca5112dfbf38cc89fb8f1
Bizhop <ville.piispa@gmail.com>
2015-09-24 14:05:37 +0000

    web stuff

--- public/js/app.js
+++ public/js/app.js
@@ -5,6 +5,7 @@ $(function() {
 	var messagesUntil = 1;
 	var chatLoaded = false;
 	var loggedUser = null;
+	var currentGame = null;
 
 	setTimeout(doPoll,1000);
 	
@@ -103,6 +104,7 @@ $(function() {
 					name = data.userName;
 					$("#chatInput").prop('disabled', false);
 					$("#loggedInEmail").html("Signed in: " + loggedUser);
+					listGames();
 				},
 				400: function(){
 					alert("Login failed");
@@ -110,6 +112,124 @@ $(function() {
 			}
 		});
 	});
+	
+	function listGames() {
+		$.ajax({
+			type: "GET",
+			url: server + "/game/getGames",
+			dataType: "json",
+			crossDomain: true,
+			statusCode: {
+				200: function(games) {
+					if(games.length > 0) {
+						for(var i=0; i < games.length; i++) {
+							var gameId = games[i];
+							var newRow = "<tr><td>" + gameId + "</td><td><button class='button' type='button' data-value='" + gameId + "'>Join</button></td></tr>";
+							$("#gameList > tbody:last").append(newRow);
+							$("button[data-value='" + gameId + "']").click(function() {
+								joinGame($(this).attr("data-value"));
+							});
+						}
+					}
+				}
+			}
+		});
+	};
+	
+	$("#roll").click(function() {
+		var gameForm = new Object();
+		gameForm.player = name;
+		gameForm.gameId = currentGame;
+		var jsonMessage = JSON.stringify(gameForm);
+		$.ajax({
+			type: "POST",
+			url: server + "/game/roll",
+			data: jsonMessage,
+			contentType: "application/json",
+			dataType: "json",
+			crossDomain: true,
+			statusCode: {
+				200: function(data) {
+					drawPlayers(game);
+				},
+				400: function(){
+					alert("Game not found (rolling)");
+				},
+				403: function() {
+					alert("Game not found (rolling)");
+				}
+			}
+		});
+	});
+	
+	function joinGame(gameId) {
+		$.ajax({
+			type: "GET",
+			url: server + "/game/get",
+			data: {"gameId": gameId},
+			dataType: "json",
+			crossDomain: true,
+			statusCode: {
+				200: function(game) {
+					$("#gameList tr").remove();
+					currentGame = game.gameId;
+					$("#gameList > tbody:last").append("Joined game " + currentGame);
+					addPlayerToGame();
+				}
+			}
+		});
+	};
+	
+	function addPlayerToGame() {
+		var gameForm = new Object();
+		gameForm.player = name;
+		gameForm.gameId = currentGame;
+		var jsonMessage = JSON.stringify(gameForm);
+		$.ajax({
+			type: "POST",
+			url: server + "/game/addPlayer",
+			data: jsonMessage,
+			contentType: "application/json",
+			dataType: "json",
+			crossDomain: true,
+			statusCode: {
+				200: function(data) {
+					drawPlayers(game);
+				},
+				400: function(){
+					alert("Game not found (adding player)");
+				}
+			}
+		});
+	};
+	
+	function drawPlayers() {
+		$.ajax({
+			type: "GET",
+			url: server + "/game/get",
+			data: {"gameId": currentGame},
+			dataType: "json",
+			crossDomain: true,
+			statusCode: {
+				200: function(game) {
+					$.each(game.players, function(k,v) {
+						var coords = v.position.coordinates;
+						var imageId = v.name + "_image";
+						$('<img />', {
+							src: '/images/token-red.png',
+							class: 'playerImages',
+							id: imageId
+						}).appendTo($("div#game"));
+						$("img#" + imageId).css("top", (coords.y - 12) + "px");
+						$("img#" + imageId).css("left", (coords.x - 12) + "px");
+					});
+				},
+				400: function() {
+					alert("Game not found (drawing players)");
+				}
+			}
+		});
+	};
 	
 	$("#register").click(function() {
 		var user = new Object();
